1b26fb9635b101fb6f6ea335ac7c090e27e76058
Gevorg Petrosyan <armen.kharatyuan@menugroup.co.uk>
2024-07-03 11:30:59 +0400

    creating meals page by adding 'better-sqlite3' package,creating initial db, making MealsPage get data and render in the server, handling styling

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+	const meals = await getMeals();
 	return (
 		<>
 			<header className={styles.header}>
@@ -9,11 +12,13 @@ export default function MealsPage() {
 					Delicious meals, created <span className={styles.highlight}> by you</span>
 				</h1>
 				<p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
-				<p>
-					<Link href="/meals/share">Sghare Your favorite recipe</Link>
+				<p className={styles.cta}>
+					<Link href="/meals/share">Share Your favorite recipe</Link>
 				</p>
 			</header>
-			<main className={styles.main}></main>
+			<main className={styles.main}>
+				<MealsGrid meals={meals} />
+			</main>
 		</>
 	);
 }
